e82090423980c3defdb28688a0278a4a5b492d65
Derek Hsieh <devderekhsieh@gmail.com>
2022-05-20 19:30:50 -0700

    Initial Commit

---
+++ FinanceStocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinanceStocks
+//
+//  Created by Derek Hsieh on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinanceStocks/FinanceStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceStocksApp.swift
+//  FinanceStocks
+//
+//  Created by Derek Hsieh on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FinanceStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
